a206d773780ddf8e8ce5fdca3aeec44ec048bfca
Arun <arun@Aruns-MacBook-Pro.local>
2025-03-12 14:28:19 -0400

    paintRequiredCalculator.test.ts: modified this file and added test cases to check the expected result and when area is  negative scenario

--- src/paintRequiredCalculator/paintRequiredCalculator.test.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.test.ts
@@ -1,9 +1,25 @@
+//goal: Calculate the paint required.
+// PaintRequired = area * coverage per liter
+
+//5000 = 500 * 10
+
+
 import paintRequiredCalculator from "./paintRequiredCalculator";
 
 describe("paint required calculator", () => {
-  test("should return expected result", () => {
-    const result = paintRequiredCalculator(50, 10);
+    test("should calculate the paint required correctly", () => {
+      const area = 500;
+      const CoveragePerLiter = 10;
+      const result = paintRequiredCalculator(area,CoveragePerLiter);
+  
+      expect(result).toBe(area*CoveragePerLiter);
+    });
+  
+    test("should throw an error when area is negative", () => {
+        const area = -500;
+        const CoveragePerLiter = 10;
+    
+        expect(() => paintRequiredCalculator(area,CoveragePerLiter).toThrow("Area should be to Positive");
 
-    expect(result).toEqual(5);
-  });
+    });
 });
